f61e2d9acaa92a57eae0279db298b1869abeef7e
bryang1099 <bryang1099@gmail.com>
2018-01-16 12:11:36 -0800

    Signed-off-by: bryang1099 <bryang1099@gmail.com>

---
+++ Lab/Lab010218/Hello_world/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: Bryan Gomez
+ * * Created on January 2, 2018, 12:34 PM
+ * Purpose: First Program Hello World
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout<<"Hello World"<<endl;
+    return 0;
+}
+
