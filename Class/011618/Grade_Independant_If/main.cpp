77295a50c11772b61a140fd1b92accf9e25cb53a
bryang1099 <bryang1099@gmail.com>
2018-01-16 11:14:03 -0800

    Signed-off-by: bryang1099 <bryang1099@gmail.com>

---
+++ Class/011618/Grade_Independant_If/main.cpp
@@ -0,0 +1,42 @@
+/* 
+ * File:   main.cpp
+ * Author: Bryan Gomez
+ * * Created on January 16, 2018, 10:05 AM
+ * Purpose: Grade with an If statement
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math/Physics Constants, Conversions,
+//                   2-D Array Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    short score;
+    char grade;
+    
+    //Initialize Variables
+    cout<<"This program gives a grade"<<endl;
+    cout<<"Type in a score from 0 to 100" <<endl;
+    cin>>score;
+    
+    //Process/Map inputs to outputs
+    if(score<0||score>100)grade='I';
+    if(score>90&&score<=100)grade='A';
+    if(score>=80&&score<90)grade='B';
+    if(score>=70&&score<80)grade='C';
+    if(score>=60&&score<70)grade='D';
+    if(score>=0&&score<60)grade='F';
+    //Output data
+    cout<<"your score ="<<score<<" and your grad e = "<<grade<<endl;
+    //Exit stage right;
+    return 0;
+}
+
---
+++ Class/011618/Grade_Switch_Case_Vi/main.cpp
@@ -0,0 +1,48 @@
+/* 
+ * File:   main.cpp
+ * Author: Bryan Gomez
+ * * Created on January 16, 2018, 10:05 AM
+ * Purpose: Grade with an Switch Case
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math/Physics Constants, Conversions,
+//                   2-D Array Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    short score;
+    char grade;
+    
+    //Initialize Variables
+    cout<<"This program gives a grade"<<endl;
+    cout<<"Type in a score from 0 to 100" <<endl;
+    cin>>score;
+    
+    //Process/Map inputs to outputs
+    grade='I';
+    switch(score/10){
+        case 10:score==100?grade='A':grade='I';break;
+        case 9:grade='A';break;
+        case 8:grade='B';break;
+        case 7:grade='C';break;
+        case 6:grade='D';break;
+        case 5:case 4:case 3:case 2:case 1:grade='F';break;
+        case 0:score<0?grade='I':grade='F';
+        default:grade='I';
+
+    }
+    //Output data
+    cout<<"your score = "<<score<<" and your grade = "<<grade<<endl;
+    //Exit stage right;
+    return 0;
+}
+
