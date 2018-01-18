f2439b841b4e65e17996d8c721548831e3773f18
Bryang1099 <bryang1099@gmail.com>
2018-01-18 11:45:28 -0800

    Signed-off-by: Bryang1099 <bryang1099@gmail.com>

---
+++ Lab/Lab011818/Savitch_9thEd_Chap3_Prob6_Fibonacci_While/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: Bryan Gomez
+ * Created on January 18, 2018, 11: PM
+ * Purpose:  Fibonacci Sequence
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Math/Physics Constants, Conversions,
+//                   2-D Array Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare Variables
+    unsigned int fi,fim1,fim2;
+    unsigned char nloops
+    
+    //Initialize Variables
+    unsigned char cnt=0;
+    while(++cnt<=nloops
+    fim1=1;
+    fim2=1;
+    cout<<fim2<<","<<fim1;
+
+    
+    //Comparison of the ratio of Fibonacci to Golden Ratio
+    cout<<endl<<endl<<"Ratio = "<<1.0f*fim1/fim2<<endl;
+    
+    //Exit stage right!
+    return 0;
+}
